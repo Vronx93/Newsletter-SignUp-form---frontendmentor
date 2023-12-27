a355225bd85933e39b55f5695fbae5458faf24c6
Vronx93 <92084864+Vronx93@users.noreply.github.com>
2023-12-27 15:02:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailValueTrim = emailValue.trim()
 if (validateEmail(emailValueTrim)) {
     localStorage.setItem('email-value', emailValue)
     invalidEmailMsg.classList.add('hidden')
-    window.location.href = 'https://github.com/Vronx93/Newsletter-SignUp-form---frontendmentor/success-msg.html'
+    window.location.href = 'https://github.io/Vronx93/Newsletter-SignUp-form---frontendmentor/success-msg.html'
     } else {
     invalidEmailMsg.classList.remove('hidden')
 }
